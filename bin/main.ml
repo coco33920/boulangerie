e559d8f078e8e0f4fdc783df1601b8a18d806a49
Charlotte Thomas <contact@nwa2coco.fr>
2023-01-03 20:21:17 +0100

    add install command (under buy)

--- bin/main.ml
+++ bin/main.ml
@@ -2,20 +2,26 @@ open Cmdliner
 
 type init_conf = { git : bool }
 type exec_conf = { verbose : bool }
-type cmd_conf = Init of init_conf | Exec of exec_conf
+type install_conf = { lib : string }
+type cmd_conf = Init of init_conf | Exec of exec_conf | Install of install_conf
 
 let run cmd =
   match cmd with
   | Init f -> Boulangerie.Initializer.init f.git
   | Exec _ -> Sys.command "baguette_sharp.repl --input lib.baguette" |> ignore
+  | Install _ -> Boulangerie.Filemanager.init ();;
 
 let git_term =
   let info = Arg.info [ "git" ] ~doc:"Initialize an empty git repository" in
   Arg.value (Arg.flag info)
 
 let bar_term =
-  let info = Arg.info [ "bar" ] ~doc:"Toggle the verbose" in
+  let info = Arg.info [ "verbose" ] ~doc:"Toggle the verbose" in
   Arg.value (Arg.flag info)
+
+let lib_term = 
+  let info = Arg.info [ "lib" ] ~doc:"Install a distant library from the repository" in 
+  Arg.value ((Arg.opt Arg.string) "" info)
 
 let init_term run =
   let combine git = Init { git } |> run in
@@ -24,6 +30,10 @@ let init_term run =
 let exec_term run =
   let combine verbose = Exec { verbose } |> run in
   Term.(const combine $ bar_term)
+
+let install_term run = 
+  let combine lib = Install { lib } |> run in 
+  Term.(const combine $ lib_term)
 
 let init_doc = "initialize an empty boulangerie project in the current folder"
 
@@ -49,9 +59,21 @@ let exec run =
   let info = Cmd.info "try" ~doc:exec_doc ~man:exec_man in
   Cmd.v info (exec_term run)
 
+let install_doc = "install the current or a distant library"
+
+let install_man = 
+  [
+    `S Manpage.s_description;
+    `P "Install the current library or the distant library in the project core"
+  ]
+
+  let install run =
+    let info = Cmd.info "buy" ~doc:install_doc ~man:install_man in 
+    Cmd.v info (install_term run)
+
 let root_doc = "The Baguette# package and project manager"
 let root_info = Cmd.info "boulangerie" ~doc:root_doc
-let subcommand run = [ init run; exec run ]
+let subcommand run = [ init run; exec run; install run ]
 
 let parse_command_line_and_run (run : cmd_conf -> unit) =
   Cmd.group root_info (subcommand run) |> Cmd.eval |> exit
---
+++ lib/filemanager.ml
@@ -0,0 +1,12 @@
+let home () = Sys.getenv "HOME";;
+let sep () = Filename.dir_sep;;
+let mkdir name = Sys.mkdir (home () ^ sep () ^ name) 0770;;
+let init_parent_file () = match Sys.file_exists (home () ^ sep () ^ ".boulangerie") with
+  | false -> mkdir ".boulangerie"
+  | true -> ();;
+
+let init_lib_file () = match Sys.file_exists (home () ^ sep () ^ ".boulangerie" ^ sep () ^ "lib") with
+  | false -> mkdir (".boulangerie" ^ sep () ^ "lib")
+  | true -> ();;
+
+let init = init_parent_file (); init_lib_file;;
