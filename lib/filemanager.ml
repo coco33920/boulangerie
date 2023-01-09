c97905786b2af777ed5c17d1b76fc3fa9262f2ea
Charlotte Thomas <contact@nwa2coco.fr>
2023-01-09 13:35:17 +0100

    parse json

--- lib/filemanager.ml
+++ lib/filemanager.ml
@@ -9,4 +9,14 @@ let init_lib_file () = match Sys.file_exists (home () ^ sep () ^ ".boulangerie" 
   | false -> mkdir (".boulangerie" ^ sep () ^ "lib")
   | true -> ();;
 
-let init = init_parent_file (); init_lib_file;;
+let init_installed_file () = match Sys.file_exists (home () ^ sep () ^ ".boulangerie" ^ sep () ^ "installed.json") with
+  | false -> let o = open_out (home () ^ sep () ^ ".boulangerie" ^ sep () ^ "installed.json") in close_out o
+  | true -> ();;
+
+let create_lib_dir name = match Sys.file_exists (home () ^ sep () ^ ".boulangerie" ^ sep () ^ "lib" ^ sep () ^ name) with
+  | false -> mkdir (".boulangerie" ^ sep () ^ "lib" ^ sep () ^ name)
+  | true -> ();;
+
+let file_in_lib_dir libname name = home () ^ sep () ^ ".boulangerie" ^ sep () ^ "lib" ^ libname ^ sep () ^ name;;
+let installed_file () = home () ^ sep () ^ ".boulangerie" ^ sep () ^ "installed.json"
+let init = init_parent_file (); init_lib_file (); init_installed_file;;
--- lib/parseboulangerie.ml
+++ lib/parseboulangerie.ml
@@ -1,6 +1,6 @@
 open Core
-open Yojson
 open Yojson.Basic
 open Yojson.Basic.Util
 
 let open_file () = from_file "_boulangerie.json";;
+let get_name json = json |> member "name" |> to_string |> String.lowercase |> String.substr_replace_all ~pattern:" " ~with_:"_";;
